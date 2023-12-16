5d8d9910db186732d41843b52094f1e2c693a846
Jupi <spacejupiter0@gmail.com>
2023-12-16 12:52:48 +0200

    added items list components

--- src/Components/ItemList/Item.tsx
+++ src/Components/ItemList/Item.tsx
@@ -1,10 +1,21 @@
+import "../../Styles/Item.css";
+
 type listItemProp = {
   text: string;
   value: string;
+  onClickItem?: (id: string) => void;
 };
 
 function Item(props: listItemProp) {
-  return <li>{props.text}</li>;
+  function onItemClick() {
+    if (props.onClickItem) props.onClickItem(props.value);
+  }
+
+  return (
+    <li class="list-item-clickable" onclick={onItemClick}>
+      {props.text}
+    </li>
+  );
 }
 
 export default Item;
--- src/Components/ItemList/ItemsList.tsx
+++ src/Components/ItemList/ItemsList.tsx
@@ -1,6 +1,7 @@
 import { For } from "solid-js";
 import Item from "./Item";
 import "../../Styles/ItemsList.css";
+import { invoke } from "@tauri-apps/api";
 
 export type itemData = {
   name: string;
@@ -12,11 +13,22 @@ type ItemsListProps = {
 };
 
 function ItemList(props: ItemsListProps) {
+  async function DeleteItem(id: string) {
+    const success = await invoke("delete_word", { id });
+    console.log(success);
+  }
+
   return (
     <div class="items-list-container">
       <ul class="items-list-ul">
         <For each={props.list}>
-          {(item) => <Item text={item.name} value={item.value} />}
+          {(item) => (
+            <Item
+              text={item.name}
+              value={item.value}
+              onClickItem={DeleteItem}
+            />
+          )}
         </For>
       </ul>
     </div>
